910800ae61c0c94e617e266481c772c197b290e9
Anas <anas.saifi4201@gmail.com>
2026-05-01 18:11:26 +0530

    Add the base url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ethara-task-manager-14sh.onrender.com/"
+  //  "http://localhost:5000/api"
 });
 
 export const setAuthToken = (token) => {
