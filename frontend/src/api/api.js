13255757ee28d7b80727d5bffb523704f368e03c
Anas <anas.saifi4201@gmail.com>
2026-05-01 18:13:12 +0530

    all done

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ethara-task-manager-14sh.onrender.com/"
+  baseURL: "https://ethara-task-manager-14sh.onrender.com/api"
   //  "http://localhost:5000/api"
 });
 
